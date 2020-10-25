3dadfe25ba8288975c8d103db5d06f58269d1e33
aliff <alifzulkifeli@gmail.com>
2020-10-26 02:59:00 +0800

    TRY IF IN COBOL

---
+++ code.cob
@@ -0,0 +1,39 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. LEARNING.
+AUTHOR. AL.
+DATE-WRITTEN. APRIL 21ST 2021
+ENVIRONMENT DIVISION.
+CONFIGURATION SECTION.
+SPECIAL-NAMES.
+       CLASS PassingScore IS "A" THRU "C, "D".
+
+DATA DIVISION.
+FILE SECTION.
+WORKING-STORAGE SECTION.
+*> variable is here
+01 Age PIC 99 VALUE 0.
+01 Grade PIC 99 VALUE 0.
+01 Score PIC X(1) VALUE "B".
+01 CanVoteFlag PIC 9 VALUE 0.
+       88 CanVote VALUE 1.
+       88 CantVote VALUE 0.
+01 TestNumber PIC X.
+       88 IsPrime VALUE "1", "3", "5", "7"
+       88 IsOdd VALUE "1", "3", "5", "7", "9"
+       88 IsEven VALUE "2", "4", "6", "8"
+       88 LessThan5 VALUE "1" THRU "4"
+       88 ANumber VALUE "1" THRU "9"
+       
+
+PROCEDURE DIVISION.
+*> code is here
+DISPLAY "Enter Age : " WITH NO ADVANCING
+ACCEPT Age
+IF Age > 18 THEN
+       DISPLAY "YOU CAN VOTE"
+ELSE 
+       DISPLAY "YOU CANNOT VOTE"
+END-IF
+
+
+STOP RUN.
